d42ec148eb08482d76c863634e5ec20e85a3af98
Audace Rabarison <audace.rabarison@hangarww.com>
2022-05-09 15:18:08 +0300

    init project

---
+++ app/src/main/java/itu/m1/edukids/MainActivity.kt
@@ -0,0 +1,11 @@
+package itu.m1.edukids
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/itu/m1/edukids/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package itu.m1.edukids.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import itu.m1.edukids.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
